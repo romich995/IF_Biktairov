ea981b2910969c727b8f8c658c2b9c60e5224b79
Biktairov Roman <rombkt@gmail.com>
2025-04-26 09:05:14 +0300

    добавил 2,3 тест

--- src/main/java/ifellow/LoginPage.java
+++ src/main/java/ifellow/LoginPage.java
@@ -1,15 +1,17 @@
 package ifellow;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class LoginPage {
+public class LoginPage extends BaseAfterLoginPage {
     private final SelenideElement loginInput = $x("//*[@id='login-form-username']");
     private final SelenideElement passwordInput = $x("//*[@id='login-form-password']");
     private final SelenideElement entryButton = $x("//*[@id='login']");
     private final SelenideElement recordAfterLogin = $x("//*[@id='gadget-10002-title']");
+    private final SelenideElement testProjectItem = $x("//a[@href='https://edujira.ifellow.ru/browse/TEST']");
 
     public LoginPage login(String login, String password) {
         loginInput.shouldBe(Condition.visible)
@@ -21,14 +23,21 @@ public class LoginPage {
         return this;
     }
 
-    public void checkLogin(){
-        recordAfterLogin.shouldBe(Condition.exist);
+    public LoginPage checkLogin(String expectedText) {
+        recordAfterLogin.shouldBe(Condition.exist)
+                .shouldHave(Condition.text(expectedText));
+        return this;
     }
 
-
-
-
-
+    public ProjectPage openTestProject() {
+        searchInput.shouldBe(Condition.visible)
+                .click();
+        testProjectItem.shouldBe(Condition.visible)
+                .click();
+        ProjectPage testProjectPage = Selenide.page(ProjectPage.class);
+        testProjectPage.setName("Test");
+        return testProjectPage;
+    }
 
 
 
---
+++ src/main/java/ifellow/ProjectPage.java
@@ -0,0 +1,37 @@
+package ifellow;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.Assertions;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class ProjectPage extends BaseAfterLoginPage{
+    @Getter
+    @Setter
+    private String name;
+
+    private final SelenideElement projectName = $x("//*[@id='project-name-val']");
+    private final SelenideElement counter = $x("//div[@class='pager']/div[@class='showing']/span") ;
+
+
+    public ProjectPage checkProjectName(String expectedText) {
+        projectName.shouldBe(Condition.visible)
+                .shouldHave(Condition.text(expectedText));
+        return this;
+    }
+
+    private int getCountTasks() {
+        return Integer.parseInt(counter.shouldBe(Condition.visible).text().split(" ")[2]);
+    }
+
+    public void checkCounter(){
+        int previousCount = getCountTasks();
+        createTask("Проверка счетчика");
+        int currentCount = getCountTasks();
+        Assertions.assertEquals(previousCount + 1 , currentCount);
+    }
+
+}
