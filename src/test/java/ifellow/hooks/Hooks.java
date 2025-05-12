76f3d002d135349d48242fcc263e7d3bfbd0ef6e
Biktairov Roman <rombkt@gmail.com>
2025-05-12 20:20:36 +0300

    добавил хук по установке конфигурации

--- src/test/java/ifellow/hooks/Hooks.java
+++ src/test/java/ifellow/hooks/Hooks.java
@@ -1,11 +1,21 @@
 package ifellow.hooks;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.PageLoadStrategy;
 
 public class Hooks {
 
-    @After("@qa")
+    @Before
+    public void setConfiguration() {
+        Configuration.pageLoadStrategy = PageLoadStrategy.EAGER.toString();
+        Configuration.timeout = 10000;
+    }
+
+    @After
     public void closeWebDriver() {
         Selenide.closeWebDriver();
     }
