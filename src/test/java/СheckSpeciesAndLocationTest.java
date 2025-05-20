ab789f52af65a971744f336c1b0acf3c184b13bb
Biktairov Roman <rombkt@gmail.com>
2025-05-20 11:49:30 +0300

    первое задание

--- src/test/java/СheckSpeciesAndLocationTest.java
+++ src/test/java/СheckSpeciesAndLocationTest.java
@@ -1,14 +1,10 @@
 import ifellow.rickandmorty.api.CharacterAPI;
-import ifellow.rickandmorty.api.EpisodeAPI;
 import ifellow.rickandmorty.dto.Character;
 import ifellow.rickandmorty.dto.Episode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.URL;
-import java.util.List;
-
-public class TaskFirstTest {
+public class СheckSpeciesAndLocationTest {
     CharacterAPI characterAPI = new CharacterAPI();
 
     @Test
